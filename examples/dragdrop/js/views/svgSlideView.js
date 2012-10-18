72bc9cc5332ed7df6614b15dd259f4a8e9c66efd
Timo Tuominen <timo.tuominen@gmail.com>
2012-10-18 15:56:29 +0200

    Add a view of small slides.

--- examples/dragdrop/js/views/svgSlideView.js
+++ examples/dragdrop/js/views/svgSlideView.js
@@ -4,6 +4,14 @@ define(["text!./svgSlideView.html"], function (template) {
 
         function show(data) {
             el.innerHTML = template;
+            var i = 0;
+            _.each(data, function (slide) {
+                el.appendChild(slide.el);
+                slide.el.className = "slide";
+                slide.el.style.left = i*50 + "px";
+                slide.el.style.top = i*50 + "px";
+                i++;
+            });
         }
         function hide() {
 
